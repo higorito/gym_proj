88941e61474faa437a08f7b94d9c8367cfdb4b76
Higor Pereira <73401043+higorito@users.noreply.github.com>
2023-08-07 14:44:41 -0300

    try catch erro email em uso

--- lib/services/auten_service.dart
+++ lib/services/auten_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthenService{
   FirebaseAuth _authFirebase = FirebaseAuth.instance;
@@ -8,9 +9,17 @@ class AuthenService{
     required String senha,
     required String usuario,
   }) async {
-      UserCredential usuarioCred = await _authFirebase.createUserWithEmailAndPassword(email: email, password: senha);
-
-      await usuarioCred.user!.updateDisplayName(usuario);
+      try {
+        UserCredential usuarioCred = await _authFirebase.createUserWithEmailAndPassword(email: email, password: senha);
+        
+        await usuarioCred.user!.updateDisplayName(usuario);
+    } on FirebaseAuthException catch (e) { //aq é o erro do firebase
+      if (e.code == "email-already-in-use") {
+        print("deu ruim nao cadastrou");  //colocaruma snackbar dps
+      }
+        
+      
+    }
   }
 
 }
